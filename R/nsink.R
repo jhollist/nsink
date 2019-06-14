e6508830818780b1c6b971e82af03da73b975bcf
Jeff Hollister <jeff.w.hollister@gmail.com>
2019-06-14 09:50:50 -0400

    Added package level documentation

---
+++ R/nsink.R
@@ -0,0 +1,14 @@
+#' nsink: A package that implements flow path analysis of nitrogen removal
+#'
+#' The N-Sink approach is based off of research outlined in
+#' \href{https://doi.org/10.1016/j.ecoleng.2010.02.006}{Kellogg et al (2010)}.
+#' This approach uses ...  Q can you add documentation here
+#'
+#' @references Kellogg, D. Q., Gold, A. J., Cox, S., Addy, K., & August, P. V.
+#'             (2010). A geospatial approach for assessing denitrification sinks
+#'             within lower-order catchments. Ecological Engineering, 36(11),
+#'             1596-1606.
+#'             \href{https://doi.org/10.1016/j.ecoleng.2010.02.006}{Link}
+#'
+#' @name nsink
+NULL
