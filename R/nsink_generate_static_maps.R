6359d1fcce68830d1a6112f83be4026abd762a09
Jeff Hollister <jeff.w.hollister@gmail.com>
2019-10-29 16:14:13 -0400

    started skelton for static map functions

---
+++ R/nsink_generate_static_maps.R
@@ -0,0 +1,13 @@
+#' Generate N-Sink Static Maps for a given HUC
+#'
+nsink_generate_static_maps <- function(){
+
+}
+
+nsink_generate_removal_map <- function(){}
+
+nsink_generate_n_loading_index <- function(){}
+
+nsink_generate_n_removal_heatmap <- function(){}
+
+nsink_generate_n_delivery_index <- function(){}
