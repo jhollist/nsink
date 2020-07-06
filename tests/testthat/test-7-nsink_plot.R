d71c868e8cb49abec605efaef072da318e529a9f
Jeff Hollister <jeff.w.hollister@gmail.com>
2020-07-06 16:21:13 -0400

    added tests for nsink_plot

--- tests/testthat/test-7-nsink_plot.R
+++ tests/testthat/test-7-nsink_plot.R
@@ -5,6 +5,9 @@ library(sf)
 load(system.file("testdata.rda", package="nsink"))
 
 test_that("plots plot", {
+  expect_null(nsink_plot(niantic_static_maps, "removal"))
+  expect_null(nsink_plot(niantic_static_maps, "transport"))
+  expect_null(nsink_plot(niantic_static_maps, "delivery"))
   expect_null(nsink_plot_delivery(niantic_static_maps$delivery_idx))
   expect_null(nsink_plot_removal(niantic_static_maps$removal_effic))
   expect_null(nsink_plot_transport(niantic_static_maps$transport_idx))
