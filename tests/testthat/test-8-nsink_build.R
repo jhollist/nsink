b68b0113fa6c8a80df9cb931288df9c24efabc2c
Jeff Hollister <jeff.w.hollister@gmail.com>
2020-07-07 16:54:40 -0400

    added a lovely space...

--- tests/testthat/test-8-nsink_build.R
+++ tests/testthat/test-8-nsink_build.R
@@ -6,6 +6,7 @@ test_that("build runs as expected", {
   library(sf)
   load(system.file("testdata.rda", package="nsink"))
   aea <- "+proj=aea +lat_0=23 +lon_0=-96 +lat_1=29.5 +lat_2=45.5 +x_0=0 +y_0=0 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs"
+
   nsink_build(nsink_get_huc_id("Niantic River")$huc_12, aea,
               output_folder = "nsink_test_data", samp_dens = 3000)
   nsink_output_tif <- list.files("nsink_test_data", ".tif")
